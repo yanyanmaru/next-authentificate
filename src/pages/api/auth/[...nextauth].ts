3d3a2a396c8d98393da906ff8f23078dacd1a25e
yanyanmaru <manmarutech@gmail.com>
2024-02-28 00:07:33 +0900

    Googleの認証を足した

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from 'next-auth/providers/google';
+
+type ClientType = {
+  clientId: string;
+  clientSecret: string;
+};
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -8,6 +15,11 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    } as ClientType),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 }
 export default NextAuth(authOptions)
